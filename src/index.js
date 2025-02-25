5964cdd3984694b18d8dc86c974b27e2f04ba77b
Harmanjot Malhi <hm21jv@hotmail.com>
2025-02-25 15:01:21 -0500

    Intialized sequelize in the project and set up the files related to it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { PORT } = require('./config');
+const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const {} = require('./controllers');
@@ -9,6 +9,6 @@ const app = express();
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-})
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Server is running on port ${ServerConfig.PORT}`);
+})   
